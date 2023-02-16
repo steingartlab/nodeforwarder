23fdeccd24031dafe18f3c4f8948a66d8f351f4b
gunnarthorsteins <gunnarthorsteins@outlook.com>
2023-02-16 10:48:46 -0500

    feat: start script w flags for docker modularity

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Initialize the variables
+serial_port=""
+baud_rate=""
+internet_port=""
+
+# Parse the command-line options
+while getopts "p:b:i" opt; do
+  case $opt in
+    p) serial_port=$OPTARG;;
+    b) baud_rate=$OPTARG;;
+    i) internet_port=$OPTARG;;
+    *) echo "Usage: $0 -p serial_port -b baud_rate -i internet_port" >&2
+       exit 1;;
+  esac
+done
+
+# Check that all variables have been provided
+if [[ -z $serial_port || -z $baud_rate || -z $internet_port]]; then
+  echo "Usage: $0 -p serial_port -b baud_rate -i internet_port" >&2
+  exit 1
+fi
+
+# This assumes npm is installedg
+npm install forever -g
+forever start nodeforwarder.js internet_port serial_port baud_rate 10000 LOG=YES
