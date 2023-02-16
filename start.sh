8c3d41029455b91af76a591127d8b7a6309eddf6
steingartlab <steingartlab@gmail.com>
2023-02-16 15:41:45 -0500

    bug: parameters

--- start.sh
+++ start.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Initialize the variables
-serial_port=""
-baud_rate=""
-internet_port=""
-
 # Parse the command-line options
-while getopts "p:b:i" opt; do
+while getopts "p:b:i:" opt; do
   case $opt in
-    p) serial_port=$OPTARG;;
-    b) baud_rate=$OPTARG;;
-    i) internet_port=$OPTARG;;
-    *) echo "Usage: $0 -p serial_port -b baud_rate -i internet_port" >&2
+    p) serial_port="$OPTARG";;
+    b) baud_rate="$OPTARG";;
+    i) internet_port="$OPTARG";;
+    \?) echo "Usage: $0 -p serial_port -b baud_rate -i internet_port" >&2
        exit 1;;
   esac
 done
 
-# This assumes npm is installed
+echo "Starting nodeforwarder.js on port $internet_port with serial port $serial_port at baud rate $baud_rate"
+
 npm install forever -g
-sudo forever start nodeforwarder.js internet_port serial_port baud_rate 10000 LOG=YES
+sudo forever start nodeforwarder.js $internet_port $serial_port $baud_rate 10000 LOG=YES
