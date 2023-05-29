8c98e9464c863f33826d7056119179fda2794d33
steingartlab <steingartlab@gmail.com>
2023-05-29 13:06:11 -0400

    feat: better logs

--- nodeforwarder.js
+++ nodeforwarder.js
@@ -1,15 +1,5 @@
 /* 
-NodeForwader: an serial to http proxy driven by ghetto get calls
-requirements 
-   -- serialport -> npm install serialport
-   -- express -> npm install express
-   -- sleep -> npm install sleep
-   -- socket.io -> npm install socket.io
-   -- cors -> npm install cors
-
-to start: node nodeforwader.js [HTTP PORT] [SERIAL PORT] [BAUD] [BUFFER LENGTH]
-to read: http://[yourip]:[spec'd port]/read/  -> returns the last [BUFFER LENGTH] bytes from the serial port as a string
-to write: http://[yourip]:[spec'd port]/write/[YOUR STRING HERE]
+to start: node nodeforwader.js [HTTP PORT] [SERIAL PORT] [BAUD]
 
 what will probably create farts/list of things to deal with later if I need to:
 - returning characters that html has issues with
@@ -25,60 +15,51 @@ TODO as of 2021-10-16:
 
 */
 
-
-parts = process.argv
+// Startup
 console.log("Starting up nodeforwarder")
+parts = process.argv
 
-if (parts.length < 5) {
-	console.log("usage: node nodeforwader.js [HTTP PORT] [SERIAL PORT] [BAUD]")
+if (parts.length < 4) {
+	console.log(`Nodeforwarder exited on startup, probably due to incorrect number of arguments passed. \
+				 Usage: node nodeforwader.js [HTTP PORT] [SERIAL PORT] [BAUD]`);
 	process.exit(1);
 }
 
-else {
-	console.log(parts);
-	httpPort = parts[2]
-	serialPort = parts[3]
-	baud = parseInt(parts[4])
+// Setup parameters
+var properties = {
+	'http': parts[2],
+	'serial': parts[3],
+	'baud': parseInt(parts[4]),
 }
-
+console.log(properties);
+var serialConnection;
 const bufferLength = 10000;
 var buffer = ""; //On Data fill a circular buffer of the specified length
 var lastHeard = 0;
 
+// Setting up server stuff
 var bodyParser = require('body-parser');
 var app = require('express')();
 var fs = require('fs');
 var cors = require('cors')
 const server = require('http').createServer(app);
 var io = require('socket.io')(server, { cors: { methods: ["GET", "POST"] } });
-server.listen(httpPort);
+server.listen(properties['http']);
 
-
-var sleep = require("sleep").sleep
+// Establish connection
 var SerialPort = require("serialport"); //per ak47 fix
-var serialPort = new SerialPort(serialPort,
-	{
-		baudRate: baud
-	});
-
-
-serialPort.on("open", function () {
-	console.log('open');
-
-});
-
-
-serialPort.on("close", function () {
+function connect() {
+	serialConnection = new SerialPort(properties['serial'], { baudRate: properties['baud'] })
+};
+connect();  // Initial ocnnection
+serialConnection.on("open", function () { console.log('serial port connection open'); });
+serialConnection.on("close", function () {
 	console.log('closed, reopening');
-	var serialPort = new SerialPort(serialPort,
-		{
-			baudRate: baud
-		});
-
+	connect()
 });
 
 
-serialPort.on('data', function (data) {
+serialConnection.on('data', function (data) {
 
 	console.log(data.toString('binary'));
 
@@ -91,24 +72,24 @@ serialPort.on('data', function (data) {
 
 //Enable Cross Site Scripting
 app.use(cors())
-
 //Allows us to rip out data
 app.use(bodyParser.urlencoded({ extended: true })); //post forms
 app.use(bodyParser.json()) // json forms (e.g. axios)
 
 
 function get_timestamp() {
-	return new Date().toISOString().slice(0, 19); // Remove milliseconds
+	return new Date().toISOString();
 }
 
 function send(url, req, res) {
 	toSend = req.originalUrl.replace(`/${url}/`, "")
 	toSend = decodeURIComponent(toSend);
 	toSend += req.originalUrl.includes("/writecf/") ? "\r\n" : "";
-	const timestamp = get_timestamp()
-	console.log(`${timestamp} sent: ${toSend}`);
-	serialPort.write(toSend);
+	serialConnection.write(toSend);
 	res.send(toSend);
+
+	const timestamp = get_timestamp();
+	console.log(`${timestamp} sent: ${toSend}`);
 }
 
 
@@ -127,16 +108,16 @@ app.post('/write', function (req, res) {
 	x = req.body
 	toSend = x['payload']
 	console.log(toSend)
-	serialPort.write(toSend)
+	serialConnection.write(toSend)
 	res.send(toSend)
 });
 
 
 //Show Last Updated
 app.get('/lastread/', function (req, res) {
-	lhs = lastHeard.toString();
-	console.log(`lastread ${lhs}`)
-	res.send(lhs)
+	lastHeard = lastHeard.toString();
+	console.log(`lastread ${lastHeard}`)
+	res.send(lastHeard)
 });
 
 
@@ -167,7 +148,7 @@ io.on('connection', function (socket) {
 	io.emit('data', buffer)
 	socket.on('input', function (msg) {
 		//console.log('message: ' + msg);
-		serialPort.write(msg + "\r\n")
+		serialConnection.write(msg + "\r\n")
 
 	});
 });
